499d092ea74c3085f1856df8afaf4240574c49e2
Jason Coleman <jason@strangerstudios.com>
2018-07-30 15:15:05 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Memberlite Dark functions and definitions
+ *
+ * @package Memberlite 4.0
+ * @subpackage Memberlite Dark 1.0
+ */
+ 
+//Enqueue scripts and styles.
+function memberlite_child_enqueue_styles() {
+    wp_enqueue_style( 'memberlite', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'memberlite_child_enqueue_styles' );
+
+//Child theme inherits parent theme settings - based on code by @greenshady from https://core.trac.wordpress.org/ticket/27177#comment:14
+function memberlite_child_switch_theme_update_mods() {
+	if ( is_child_theme() && false === get_theme_mods() ) {
+		$mods = get_option( 'theme_mods_' . get_option( 'template' ) );
+		if ( false !== $mods ) {
+			foreach ( (array) $mods as $mod => $value ) {
+				if ( 'sidebars_widgets' !== $mod )
+					set_theme_mod( $mod, $value );
+			}
+		}
+	}
+}
+add_action( 'switch_theme', 'memberlite_child_switch_theme_update_mods' );
